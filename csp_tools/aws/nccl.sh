22b18e9d85ef564a513fa7074cdcbc7f0260cbd9
AWS ParallelCluster user <ubuntu@ip-10-0-124-123.ec2.internal>
2023-05-11 23:28:17 +0000

    add pmix flag

--- csp_tools/aws/nccl.sh
+++ csp_tools/aws/nccl.sh
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#SBATCH --gpus-per-node=8
-#SBATCH --ntasks-per-node=8
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=00:20:00
 
 export NCCL_TOPO_FILE=/nccl/p4d-24xl-topo.xml
@@ -22,7 +22,7 @@ export NCCL_TOPO_FILE=/nccl/p4d-24xl-topo.xml
 env | grep "SLURMD_NODENAME="
 env | grep "SLURM_NODELIST="
 
-srun --container-image=../../nemo_megatron_training.sqsh \
+srun --mpi=pmix --container-image=../../nemo_megatron_training.sqsh \
      --container-mounts="$PWD:/nccl" \
      bash -c "
      /nccl/nccl-tests/build/all_reduce_perf -b 256M -e 8G -f 2 -c 1 -n 10"
