60b65072dfe9e8fa393d32fee6d118e322f35aa4
Sangkug Lym <slym@nvidia.com>
2023-06-05 17:28:47 -0700

    mpi setting with UB-based TP-overlap
    Signed-off-by: Sangkug Lym <slym@nvidia.com>

--- launcher_scripts/nemo_launcher/core/stages.py
+++ launcher_scripts/nemo_launcher/core/stages.py
@@ -248,6 +248,10 @@ class NemoMegatronStage:
         }
         if cluster == "bcm":
             cluster_cfg = cfg.get("cluster")
+            if cfg.get("training").get("model").get("ub_tp_comm_overlap", False):
+                if "srun_args" not in cluster_cfg:
+                    cluster_cfg["srun_args"] = []
+                cluster_cfg["srun_args"] += ["--mpi pmix"]
             slurm_cfg = {**copy.deepcopy(cluster_cfg)}
             job_name_prefix = slurm_cfg.pop("job_name_prefix")
             cluster_parameters = {**slurm_cfg}
