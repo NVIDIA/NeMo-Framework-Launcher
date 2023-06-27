cfc0b310fea3cdd1d5aa40111b7c7f3b928523b3
Joseph Jennings <jjennings@nvidia.com>
2023-06-26 18:22:02 -0700

    Add Support for ndc modules

--- launcher_scripts/main.py
+++ launcher_scripts/main.py
@@ -18,7 +18,12 @@ import sys
 import hydra
 import omegaconf
 from nemo_launcher.core.data_stages import CustomDataPreparation, MC4DataPreparation, PileDataPreparation
-from nemo_launcher.core.data_curation_stages import CommonCrawl
+from nemo_launcher.core.data_curation_stages import (
+    CommonCrawl,
+    Filter,
+    TextCleaning,
+    LangIDAndSeparation,
+)
 from nemo_launcher.core.export_stages import Export
 from nemo_launcher.core.stages import (
     AdapterLearning,
@@ -31,9 +36,21 @@ from nemo_launcher.core.stages import (
     Training,
 )
 
-omegaconf.OmegaConf.register_new_resolver("multiply", lambda x, y: x * y, replace=True)
-omegaconf.OmegaConf.register_new_resolver("divide_ceil", lambda x, y: int(math.ceil(x / y)), replace=True)
-omegaconf.OmegaConf.register_new_resolver("divide_floor", lambda x, y: int(math.floor(x / y)), replace=True)
+omegaconf.OmegaConf.register_new_resolver(
+    "multiply",
+    lambda x, y: x * y,
+    replace=True,
+)
+omegaconf.OmegaConf.register_new_resolver(
+    "divide_ceil",
+    lambda x, y: int(math.ceil(x / y)),
+    replace=True,
+)
+omegaconf.OmegaConf.register_new_resolver(
+    "divide_floor",
+    lambda x, y: int(math.floor(x / y)),
+    replace=True,
+)
 
 STR2STAGECLASS = {
     "training": Training,
@@ -45,20 +62,27 @@ STR2STAGECLASS = {
     "export": Export,
     "evaluation": {
         EvalHarnessEvaluation: ["gpt3", "prompt_gpt3"],
-        NeMoEvaluation: ["t5", "mt5", "prompt_t5", "prompt_mt5", "adapter_t5", "adapter_gpt3", "ia3_t5", "ia3_gpt3"],
+        NeMoEvaluation: [
+            "t5", "mt5", "prompt_t5", "prompt_mt5", "adapter_t5",
+            "adapter_gpt3", "ia3_t5", "ia3_gpt3"
+        ],
     },
     "data_preparation": {
         PileDataPreparation: ["gpt3", "t5", "bert"],
         MC4DataPreparation: ["mt5"],
         CustomDataPreparation: ["generic"],
     },
-    "data_curation": CommonCrawl,
+    'download_common_crawl': CommonCrawl,
+    'filter': Filter,
+    'lang_id': LangIDAndSeparation,
+    'text_cleaning': TextCleaning,
 }
 
 
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg):
     requested_stages = cfg.get("stages")
+    #breakpoint()
 
     dependency = None
     for stage_name in requested_stages:
--- launcher_scripts/nemo_launcher/core/data_curation_stages.py
+++ launcher_scripts/nemo_launcher/core/data_curation_stages.py
@@ -1,4 +1,5 @@
 import copy
+import shlex
 import omegaconf
 from typing import Dict
 from pathlib import Path
@@ -11,15 +12,16 @@ from nemo_launcher.core.launchers import AutoLauncher
 
 
 class DataCurationStage(NemoMegatronStage):
+
     def __init__(self, cfg):
         super().__init__(cfg)
         self.log_folder = Path()
         self.conf_folder = Path()
+        self._ndc_path = "/opt/nemo-data-curator"
+        self._ndc_script_name = None
 
     def setup_stage_vars(self, cfg):
-        """Setup the stage vars, i.e. stage name and stage cfg"""
-        self.stage_name = "data_curation"
-        self.stage_cfg = cfg.get("data_curation")
+      raise NotImplementedError
 
     def setup_folder_and_data(self) -> None:
         job_path = self.get_job_path()
@@ -34,9 +36,75 @@ class DataCurationStage(NemoMegatronStage):
         self.conf_folder = Path(job_path.folder, 'conf')
         self.conf_folder.mkdir(parents=True, exist_ok=True)
 
+    def create_copy_command(self) -> None:
+        cfg = self.cfg
+        job_path = self.get_job_path()
+        container = cfg.get('container')
 
-class CommonCrawl(DataCurationStage):
-    # Does not have sub stages, just multiple command groups
+        mounts = self._make_container_mounts_string()
+        script_path = f"{self._ndc_path}/examples/{self._ndc_script_name}"
+
+        copy_command = shlex.join([
+            'srun',
+            '--nodes=1',
+            f'--container-image={container}',
+            f'--container-mounts={mounts}',
+            'bash',
+            '-c',
+            f'cp {script_path} {job_path.folder}',
+        ])
+
+        return copy_command
+
+    def create_ndc_example_command(self, ) -> str:
+      raise NotImplementedError
+
+    def _make_cluster_parameters(
+        self,
+        cluster: str,
+    ) -> Dict:
+        """
+        Make a cluster-specific parameters for jobs on different clusters.
+        Current clusters include bcm(slurm), bcp and interactive.
+        For example for bcm, it will return slurm parameters:
+            {'job_name': 'some_name', 'nodes': 2, 'ntasks_per_node': 8, ...}
+
+        :param str cluster: i.e. `bcm`, `bcp`, `interactive`, etc.
+        :param Optional sub_stage: current sub_stage name
+        :return: a dictionary of cluster parameters, e.g. `ntasks_per_node`
+        :rtype: Dict
+        """
+        cfg = self.cfg
+        stage_cfg = self.stage_cfg
+
+        run_cfg = stage_cfg.get("run")
+        job_name = run_cfg.get("name")
+        time_limit = run_cfg.get("time_limit")
+        nodes = run_cfg.get('nodes')
+        # Allow for updating the partition as we might run
+        # on CPU only nodes
+        partition = run_cfg.get('partition')
+
+        shared_parameters = {
+            "job_name": job_name,
+            "time": time_limit,
+        }
+        if cluster == "bcm":
+            cluster_cfg = cfg.get("cluster")
+            slurm_cfg = {**copy.deepcopy(cluster_cfg)}
+            job_name_prefix = slurm_cfg.pop("job_name_prefix")
+            cluster_params = {
+                **slurm_cfg,
+            }
+            cluster_params.update({
+                **shared_parameters,
+            })
+            cluster_params[
+                "job_name"] = job_name_prefix + cluster_params["job_name"]
+            cluster_params['nodes'] = nodes
+            cluster_params['partition'] = partition
+
+        return cluster_params
 
     def run(self) -> str:
         # Create the log and res dir
@@ -47,8 +115,15 @@ class CommonCrawl(DataCurationStage):
         # Add the command to setup
         # This is because all of the srun calls are already described
         # within the example scripts in the NeMo Data Curator
-        command = self._create_download_command()
-        cluster_parameters['setup'] = [command]
+        # TODO: handle the case of changing arguments by removing those
+        #       lines from the script
+        #       so python will take a "template" script and modify it
+        #       based on boolean arguments
+        #       need a command that first copies the template
+        #       out of the container
+        copy_command = self.create_copy_command()
+        command = self.create_ndc_example_command()
+        cluster_parameters['setup'] = [copy_command, command]
 
         # Create launcher
         launcher = AutoLauncher(
@@ -60,7 +135,19 @@ class CommonCrawl(DataCurationStage):
 
         return job_id
 
-    def _create_download_command(self, ) -> str:
+
+class CommonCrawl(DataCurationStage):
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self._ndc_script_name = 'download_common_crawl.sh'
+
+    def setup_stage_vars(self, cfg):
+        """Setup the stage vars, i.e. stage name and stage cfg"""
+        self.stage_name = "common_crawl"
+        self.stage_cfg = cfg.get("common_crawl")
+
+    def create_ndc_example_command(self, ) -> str:
         cfg = self.cfg
         stage_cfg = self.stage_cfg
         run_cfg = stage_cfg.get("run")
@@ -90,47 +177,133 @@ class CommonCrawl(DataCurationStage):
 
         # Add the command
         cmd = download_args + [
-            'bash', '/opt/nemo-data-curator/examples/download_common_crawl.sh'
+            'bash', f'{job_path.folder}/{self._ndc_script_name}'
         ]
         return " ".join(cmd)
 
-    def _make_cluster_parameters(
-        self,
-        cluster: str,
-    ) -> Dict:
-        """
-        Make a cluster-specific parameters for jobs on different clusters.
-        Current clusters include bcm(slurm), bcp and interactive.
-        For example for bcm, it will return slurm parameters:
-            {'job_name': 'some_name', 'nodes': 2, 'ntasks_per_node': 8, ...}
 
-        :param str cluster: i.e. `bcm`, `bcp`, `interactive`, etc.
-        :param Optional sub_stage: current sub_stage name
-        :return: a dictionary of cluster parameters, e.g. `ntasks_per_node`
-        :rtype: Dict
-        """
+class Filter(DataCurationStage):
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self._ndc_script_name = 'filter_documents.sh'
+
+    def setup_stage_vars(self, cfg):
+        """Setup the stage vars, i.e. stage name and stage cfg"""
+        self.stage_name = "filter"
+        self.stage_cfg = cfg.get("filter")
+
+    def create_ndc_example_command(self) -> str:
         cfg = self.cfg
         stage_cfg = self.stage_cfg
-
         run_cfg = stage_cfg.get("run")
-        job_name = run_cfg.get("name")
-        time_limit = run_cfg.get("time_limit")
+        job_path = self.get_job_path()
 
-        shared_parameters = {
-            "job_name": job_name,
-            "time": time_limit,
-        }
-        if cluster == "bcm":
-            cluster_cfg = cfg.get("cluster")
-            slurm_cfg = {**copy.deepcopy(cluster_cfg)}
-            job_name_prefix = slurm_cfg.pop("job_name_prefix")
-            cluster_params = {
-                **slurm_cfg,
-            }
-            cluster_params.update({
-                **shared_parameters,
-            })
-            cluster_params[
-                "job_name"] = job_name_prefix + cluster_params["job_name"]
+        # Write out the filter configuration as a separate config file
+        filter_cfg = Path(self.conf_folder, f"{self.job_name}_filter.yaml")
+        omegaconf.OmegaConf.save(stage_cfg.get('filter'), filter_cfg)
 
-        return cluster_params
+        filter_args = create_args_list(
+            log_dir=self.log_folder,
+            res_dir=job_path.results_folder,
+            conf_dir=self.conf_folder,
+            cpus_per_node=run_cfg.get('cpus_per_node'),
+            docker_image=cfg.get('container'),
+            mounts=self._make_container_mounts_string(),
+            input_dir=stage_cfg.get('input_dir'),
+            output_dir=stage_cfg.get('output_dir'),
+            filter_config_file=filter_cfg,
+            replace_underscore=False,
+        )
+        filter_args = [arg.replace('--', '') for arg in filter_args]
+
+        # Add the command
+        cmd = filter_args + [
+            'bash', f'{job_path.folder}/{self._ndc_script_name}'
+        ]
+        return " ".join(cmd)
+
+
+class TextCleaning(DataCurationStage):
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self._ndc_script_name = 'text_cleaning.sh'
+
+    def setup_stage_vars(self, cfg):
+        """Setup the stage vars, i.e. stage name and stage cfg"""
+        self.stage_name = "text_cleaning"
+        self.stage_cfg = cfg.get("text_cleaning")
+
+    def create_ndc_example_command(self) -> str:
+        cfg = self.cfg
+        stage_cfg = self.stage_cfg
+        run_cfg = stage_cfg.get("run")
+        job_path = self.get_job_path()
+
+        filter_args = create_args_list(
+            log_dir=self.log_folder,
+            res_dir=job_path.results_folder,
+            conf_dir=self.conf_folder,
+            cpus_per_node=run_cfg.get('cpus_per_node'),
+            docker_image=cfg.get('container'),
+            mounts=self._make_container_mounts_string(),
+            input_dir=stage_cfg.get('input_dir'),
+            output_dir=stage_cfg.get('output_dir'),
+            replace_underscore=False,
+        )
+        filter_args = [arg.replace('--', '') for arg in filter_args]
+
+        # Add the command
+        cmd = filter_args + [
+            'bash', f'{job_path.folder}/{self._ndc_script_name}'
+        ]
+        return " ".join(cmd)
+
+
+class LangIDAndSeparation(DataCurationStage):
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self._ndc_script_name = 'lang_id_and_separation.sh'
+
+    def setup_stage_vars(self, cfg):
+        """Setup the stage vars, i.e. stage name and stage cfg"""
+        self.stage_name = "lang_id"
+        self.stage_cfg = cfg.get("lang_id")
+
+    def create_ndc_example_command(self) -> str:
+        cfg = self.cfg
+        stage_cfg = self.stage_cfg
+        run_cfg = stage_cfg.get("run")
+        job_path = self.get_job_path()
+
+        # Write out the lang id config as a separate config
+        lang_id_cfg = stage_cfg.get('lang_id')
+        # Rename the parameters so filter_documents can read it
+        filter_cfg = {}
+        filter_cfg['filter_module'] = lang_id_cfg['lang_id_module']
+        filter_cfg['params'] = lang_id_cfg['params']
+
+        output_cfg = Path(self.conf_folder, f"{self.job_name}_lang_id.yaml")
+        omegaconf.OmegaConf.save(filter_cfg, output_cfg)
+
+        filter_args = create_args_list(
+            log_dir=self.log_folder,
+            res_dir=job_path.results_folder,
+            conf_dir=self.conf_folder,
+            cpus_per_node=run_cfg.get('cpus_per_node'),
+            docker_image=cfg.get('container'),
+            mounts=self._make_container_mounts_string(),
+            input_dir=stage_cfg.get('input_dir'),
+            output_dir=stage_cfg.get('output_dir'),
+            filter_config_file=output_cfg,
+            replace_underscore=False,
+        )
+        filter_args = [arg.replace('--', '') for arg in filter_args]
+
+        # Add the command
+        cmd = filter_args + [
+            'bash', f'{job_path.folder}/{self._ndc_script_name}'
+        ]
+        return " ".join(cmd)
