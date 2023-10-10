01dc89c5e8d2de0767d29406acfb9fccebcd94ec
Akshit Arora <akshita@nvidia.com>
2023-10-10 15:35:10 -0700

    added sample cluster validation output

--- csp_tools/aws/nccl.sh
+++ csp_tools/aws/nccl.sh
@@ -17,12 +17,54 @@
 #SBATCH --ntasks-per-node=8
 #SBATCH --time=00:20:00
 
-export NCCL_TOPO_FILE=/nccl/p4d-24xl-topo.xml
+# Load libraries
+export NCCL_TOPO_FILE=/nccl/topo.xml
+export 
+LD_LIBRARY_PATH=/opt/nccl/build/lib:/usr/local/cuda/lib64:/opt/amazon/efa/lib64:/opt/amazon/openmpi/lib:/opt/aws-ofi-nccl/lib:$LD_LIBRARY_PATH
+
+# EFA configurations
+export FI_PROVIDER=efa
+
+# NCCL configurations
+export NCCL_DEBUG=info
+export NCCL_PROTO=simple
+export NCCL_BUFFSIZE=33554432
+export NCCL_ALGO=ring
+export NCCL_DEBUG_SUBSYS=init,net,graph,env
+export NCCL_MIN_NCHANNELS=32
+
+# pmix conf
+export PMIX_MCA_gds=^ds12
+export OMPI_MCA_btl=tcp,self
+export OMPI_MCA_pml=^cm
+export OMPI_MCA_btl_tcp_if_exclude=lo,docker0
+export PMIX_MCA_btl=tcp,self
+export PMIX_MCA_pml=^cm
+export PMIX_MCA_btl_tcp_if_exclude=lo,docker0
 
 env | grep "SLURMD_NODENAME="
 env | grep "SLURM_NODELIST="
 
-srun --mpi=pmix --container-image=../../nemo_megatron_training.sqsh \
+module load openmpi
+srun --mpi=pmix --nodes=2 --tasks-per-node=8 --container-image=../../nemo_megatron_training.sqsh \
      --container-mounts="$PWD:/nccl" \
      bash -c "
-     /nccl/nccl-tests/build/all_reduce_perf -b 256M -e 8G -f 2 -c 1 -n 10"
+     NCCL_PROTO=simple \
+     NCCL_DEBUG=info \
+     NCCL_MIN_NCHANNELS=32 \
+     NCCL_BUFFSIZE=33554432 \
+     OPAL_PREFIX=/opt/amazon/openmpi \
+     NCCL_DEBUG_SUBSYS=init,net,graph,env \
+     FI_PROVIDER=efa \
+     NCCL_ALGO=ring \
+     PMIX_MCA_gds=^ds12 \
+     OMPI_MCA_btl=tcp,self \
+     OMPI_MCA_pml=^cm \
+     OMPI_MCA_btl_tcp_if_exclude=lo,docker0 \
+     PMIX_MCA_btl=tcp,self \
+     PMIX_MCA_pml=^cm \
+     PMIX_MCA_btl_tcp_if_exclude=lo,docker0 \
+     LD_LIBRARY_PATH="/opt/nccl/build/lib:/usr/local/cuda/lib64:/opt/amazon/efa/lib64:/opt/amazon/openmpi/lib:/opt/aws-ofi-nccl/lib:$LD_LIBRARY_PATH" \
+     PATH="/usr/local/nvm/versions/node/v16.19.1/bin:/opt/amazon/efa/bin:/usr/local/lib/python3.8/dist-packages/torch_tensorrt/bin:/opt/amazon/openmpi/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/ucx/bin:/opt/tensorrt/bin:/opt/s5cmd" \
+     NCCL_TOPO_FILE="/nccl/topo.xml" \
+     /nccl/nccl-tests/build/all_reduce_perf -b 8 -e 16G -f 2 -g 1 -c 0 -n 100"
