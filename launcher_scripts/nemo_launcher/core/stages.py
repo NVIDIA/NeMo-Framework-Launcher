f317fcea92bc8d829cbb991cc18b8c15608bab1f
HuiyingLi <willwin.lee@gmail.com>
2023-11-20 10:14:01 -0800

    add falcon sft peft stage

--- launcher_scripts/nemo_launcher/core/stages.py
+++ launcher_scripts/nemo_launcher/core/stages.py
@@ -878,6 +878,8 @@ class FineTuning(NeMoStage):
             / "examples/nlp/language_modeling/megatron_t5_seq2seq_finetune.py",
             "mt5": self._nemo_code_path
             / "examples/nlp/language_modeling/megatron_t5_seq2seq_finetune.py",
+            "falcon": self._nemo_code_path
+            / "examples/nlp/language_modeling/tuning/megatron_gpt_sft.py",
         }
         return model_type_to_code_path[model_type]
 
@@ -924,6 +926,8 @@ class PEFT(NeMoStage):
             / "examples/nlp/language_modeling/tuning/megatron_gpt_peft_tuning.py",
             "t5": self._nemo_code_path
             / "examples/nlp/language_modeling/tuning/megatron_t5_peft_tuning.py",
+            "falcon": self._nemo_code_path
+            / "examples/nlp/language_modeling/tuning/megatron_gpt_peft_tuning.py"
         }
         return model_type_to_code_path[model_type]
 
