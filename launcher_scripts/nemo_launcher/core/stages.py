2dd451ba37489c89e55a8f19c09670fdc9f66ad4
Huy Vu2 <huvu@login-eos01.eos.clusters.nvidia.com>
2024-02-02 08:08:58 -0800

    adding files for mcore retro

--- launcher_scripts/nemo_launcher/core/stages.py
+++ launcher_scripts/nemo_launcher/core/stages.py
@@ -815,7 +815,8 @@ class Training(NeMoStage):
             / "examples/nlp/language_modeling/megatron_bert_pretraining.py",
             "falcon": self._nemo_code_path
             / "examples/nlp/language_modeling/megatron_gpt_pretraining.py",
-        }
+            "retro": self._nemo_code_path
+            / "examples/nlp/language_modeling/megatron_retro_pretraining.py",        }
         return model_type_to_code_path[model_type]
 
 
