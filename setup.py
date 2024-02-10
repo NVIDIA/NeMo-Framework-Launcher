fd3240075c191d6e2ab691d9669a89d86bd77238
Terry Kong <terryk@nvidia.com>
2024-02-09 16:10:14 -0800

    black formatter

--- setup.py
+++ setup.py
@@ -1,32 +1,26 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='nemo_megatron_launcher',
-    version='0.0.0',
-    author='NVIDIA',
-    description='NeMo Megatron launcher and tools',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/NVIDIA/NeMo-Megatron-Launcher',
+    name="nemo_megatron_launcher",
+    version="0.0.0",
+    author="NVIDIA",
+    description="NeMo Megatron launcher and tools",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NVIDIA/NeMo-Megatron-Launcher",
     packages=find_packages(),
     install_requires=requirements,
     extras_require={
-        'lint': [
-            'black==19.10b0',
-            'click==8.0.2',
-        ],
-        'test': [
-            'pytest',
-            'requests-mock',
-        ],
+        "lint": ["black==19.10b0", "click==8.0.2",],
+        "test": ["pytest", "requests-mock",],
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
