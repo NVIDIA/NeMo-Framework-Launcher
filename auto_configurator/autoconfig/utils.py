49439bc1b5a02e0a725d0385fd7e2bf3a6e8e035
Euynaheh <93857693+Euynaheh@users.noreply.github.com>
2024-02-22 14:10:39 +0800

    Add baichuan2 support (#210)
    * update container path
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    Signed-off-by: Euynaheh <hehanyue99@outlook.com>
    
    * change container path
    
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>
    Signed-off-by: Euynaheh <hehanyue99@outlook.com>
    
    * Add Baichuan2 support
    
    Signed-off-by: Euynaheh <hehanyue99@outlook.com>
    
    * Add Baichuan2 auto configurator
    
    Signed-off-by: Euynaheh <hehanyue99@outlook.com>
    
    * add baichuan2 support
    
    * fix baichuan2 entrance
    
    * fix code format
    
    * fix conflicts
    
    * fix conflict
    
    * fix code style
    
    * fix code format
    
    ---------
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    Signed-off-by: Euynaheh <hehanyue99@outlook.com>
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>
    Co-authored-by: dimapihtar <dpihtar@gmail.com>
    Co-authored-by: dimapihtar <dpykhtar@nvidia.com>
    Co-authored-by: Eric Harper <complex451@gmail.com>
    Co-authored-by: Dmytro Pykhtar <37850217+dimapihtar@users.noreply.github.com>

--- auto_configurator/autoconfig/utils.py
+++ auto_configurator/autoconfig/utils.py
@@ -45,7 +45,7 @@ def _calculate_model_size(
     :rtype: float
     :raises NotImplementedError: if the model name is not valid.
     """
-    if model_name in ["gpt3", "llama"]:
+    if model_name in ["gpt3", "llama", "baichuan2"]:
         model_size = (
             12
             * num_layers
@@ -113,7 +113,7 @@ def calculate_model_size_params(
     :raises NotImplementedError: if the model name is not supported.
     """
     ffn, kv = None, None  # Only needed for some models.
-    if model_name in ["gpt3", "llama"]:
+    if model_name in ["gpt3", "llama", "baichuan2"]:
         if model_size_in_b < 0.25:
             hs, att_h, lr = 768, 12, 6e-4
         elif model_size_in_b < 0.5:
@@ -395,28 +395,38 @@ def modify_cfg(
     """
     new_cfg = copy.deepcopy(base_cfg)
     if act is not None:
-        if model_name in ["gpt3", "bert", "llama"]:
+        if model_name in ["gpt3", "bert", "llama", "baichuan2"]:
             new_cfg["model"]["activations_checkpoint_num_layers"] = act
         else:
             new_cfg["model"]["encoder"]["activations_checkpoint_num_layers"] = act // 2
             new_cfg["model"]["decoder"]["activations_checkpoint_num_layers"] = act // 2
 
-    if num_mbs_act is not None and model_name in ["gpt3", "bert", "llama"]:
+    if num_mbs_act is not None and model_name in ["gpt3", "bert", "llama", "baichuan2"]:
         new_cfg["model"][
             "num_micro_batches_with_partial_activation_checkpoints"
         ] = num_mbs_act
 
-    if act_per_pipe is not None and model_name in ["gpt3", "bert", "llama"]:
+    if act_per_pipe is not None and model_name in [
+        "gpt3",
+        "bert",
+        "llama",
+        "baichuan2",
+    ]:
         new_cfg["model"]["activations_checkpoint_layers_per_pipeline"] = act_per_pipe
 
-    if virtual_pipelines is not None and model_name in ["gpt3", "bert", "llama"]:
+    if virtual_pipelines is not None and model_name in [
+        "gpt3",
+        "bert",
+        "llama",
+        "baichuan2",
+    ]:
         new_cfg["model"]["virtual_pipeline_model_parallel_size"] = virtual_pipelines
 
     new_cfg["model"]["tensor_model_parallel_size"] = tp
     new_cfg["model"]["pipeline_model_parallel_size"] = pp
     new_cfg["model"]["micro_batch_size"] = mbs
 
-    if model_name in ["gpt3", "bert", "llama"]:
+    if model_name in ["gpt3", "bert", "llama", "baichuan2"]:
         att_heads = new_cfg["model"]["num_attention_heads"]
         num_layers = new_cfg["model"]["num_layers"]
     else:
