b5aea9b54645ef835cc554535744361eb15b4720
Ao Tang <aot@nvidia.com>
2024-03-14 11:09:33 -0700

    add space

--- launcher_scripts/main.py
+++ launcher_scripts/main.py
@@ -121,6 +121,7 @@ def main(cfg):
     dependency = None
     for stage_name in requested_stages:
         stage_class = STR2STAGECLASS[stage_name]
+
         if isinstance(stage_class, dict):
             stage_config_choice = cfg.get(f"{stage_name}_config")
             choice_model_type = stage_config_choice.rsplit("/", 1)[0]
